446238aa9dc784bc7727ada681d91a6e66fd8db2
mmmmmmmmak <88006224+mmmmmmmmak@users.noreply.github.com>
2022-08-02 23:23:05 +0300

    Delete interfaces directory

--- interfaces/IQuestionMessageContext.d.ts
+++
@@ -1,8 +0,0 @@
-import { Answer } from '../structures';
-export interface IQuestionMessageContext {
-    /**
-     * @param message Текст вопроса
-     * @param params Параметры отправляемого сообщения
-     */
-    question: (message: string) => Promise<Answer>;
-}
